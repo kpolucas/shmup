2c70a3f84078eb6e841e5a96849589467732cc48
chorga <kpolucas88@gmail.com>
2019-08-09 17:29:15 -0300

    primer commit

---
+++ Move2D.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    public float playerSpeed = 6f;
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        transform.position += movement * Time.deltaTime * playerSpeed;
+    }
+}
---
+++ Shoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    float timeStamp = 0;
+    int weapon = 1;
+
+    void Update()
+    {
+        /* La onda seria algo asi
+         * timeStamp = bala.Shoot(timeStamp,lvlDeLaBala)
+         * 
+         * y en Shoot del script Bala
+         * if(timeStamp < time.time)
+         *  logica de arma en particular
+         *  timeStamp = time.time + cooldown
+         *  return timeStamp
+         */
+    }
+}
