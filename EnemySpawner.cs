5a5f0cf92704dabc472dd828e0a07ea593ac3edf
chorga <kpolucas88@gmail.com>
2019-08-14 21:42:42 -0300

    agregado spawner

---
+++ EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public int cantidad;
+    public float intervEnSec;
+    public GameObject enemyType;
+    //public anim queanimacion;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "EnemyTrigger")
+        {
+            Debug.Log(other.name);
+            /*
+            GameObject bullet = Instantiate(bulletMGunLVL1, firePoint.position, firePoint.rotation);
+            bullet.name = "Bullet";
+            */
+        }
+    }
+
+
+}
